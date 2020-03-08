0f17569b958c65dcc6e8754eee64ef80a2bc0f88
Christopher Cid <iamcid17@gmail.com>
2020-03-08 23:30:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   sodas = ["coke", "pepsi", "sprite", "ginger ale"]
-  sodas.unshift
+  sodas.unshift ("wow")
 end
 
 def remove_element_from_end_of_array(array)
