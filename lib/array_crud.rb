cf93086c39e6161804f6dcd53bba2e85612ff643
Christopher Cid <iamcid17@gmail.com>
2020-03-08 23:33:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   sodas = ["coke", "pepsi", "sprite", "ginger ale"]
-  ginger_ale = 
+  ginger_ale = soda.pop
 end
 
 def remove_element_from_start_of_array(array)
