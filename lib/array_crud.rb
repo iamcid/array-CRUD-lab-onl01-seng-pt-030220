7a61da96c2ce18023c3302e40c670555d4f017d4
Christopher Cid <iamcid17@gmail.com>
2020-03-08 23:35:35 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,7 @@ end
 def remove_element_from_end_of_array(array)
   sodas = ["coke", "pepsi", "sprite", "ginger ale"]
   arrays = sodas.pop
-  puts "arrays!"
+  return "arrays!"
 end
 
 def remove_element_from_start_of_array(array)
