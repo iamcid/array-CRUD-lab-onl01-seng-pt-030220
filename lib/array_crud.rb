8b81361145d618ff5eeac0b9a598e7b453cb1f19
Christopher Cid <iamcid17@gmail.com>
2020-03-08 23:37:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,9 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  sodas = ["coke", "pepsi", "sprite", "ginger ale"]
+  wow = sodas.shift
+  return "wow"
 end
 
 def retrieve_element_from_index(array, index_number)
