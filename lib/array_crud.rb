7386fdc38ed925d3a6a3828c3f46b0737106054d
Christopher Cid <iamcid17@gmail.com>
2020-03-08 23:27:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   sodas = ["coke","pepsi", "sprite", "ginger ale"]
-  sodas << "arrays!"
+  sodas << "root beer!"
 end
 
 def add_element_to_start_of_array(array, element)
